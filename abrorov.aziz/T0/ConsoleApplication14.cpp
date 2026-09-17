30ef57bc6d3785a8b33347c90c862b807312be5f
Aziz <abrorov.at@spbstu.ru>
2026-09-17 18:40:51 +0300

    init

---
+++ abrorov.aziz/T0/ConsoleApplication14.cpp
@@ -0,0 +1,10 @@
+﻿// ConsoleApplication14.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "abrorov.aziz\n";
+   
+}
